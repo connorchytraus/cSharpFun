4727a5261d11deae18d379ec4e2d4f0e892baf3a
Connor <Connor@CONNORCHYTRAUS>
2023-01-17 15:33:20 -0700

    Add project files.

---
+++ cSharpFun/PrintStuff.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cSharpFun
+{
+    public class PrintStuff
+    {
+        private string language = "";
+        public PrintStuff(string temp)
+        {
+            language = temp;
+        }
+
+        public void PrintName(string name)
+        {
+            if (language == "EN")
+            {
+                Console.WriteLine("Hello, " + name);
+            }
+
+            if (language == "KH")
+            {
+                Console.WriteLine("Suarsday " + name);
+            }
+            
+        }
+    }
+}
---
+++ cSharpFun/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace cSharpFun
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintStuff ps = new PrintStuff("KH"); //instantiation//declaration
+           
+
+            string ?userName = "";
+            Console.Write("Please enter your name: ");
+            userName = Console.ReadLine();
+
+            ps.PrintName(userName);
+        }
+
+
+    }
+}
